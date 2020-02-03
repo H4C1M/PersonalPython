ff79ddd4ac6b7e517efc6470f6b1842444a5c319
h4c1m <micahvargas@icloud.com>
2020-02-03 14:14:40 -0500

    Initial commit

---
+++ src/Testing.py
@@ -0,0 +1,13 @@
+
+myStr = "Hello World"
+
+print(myStr)
+
+# raw_input is for strings
+firstName = raw_input("Enter First Name: ")
+lastName = raw_input("Enter Last Name: ")
+age = input('Enter Age: ')
+# ' ' is same as " "
+# input is for integers
+
+print("I am " + firstName + " " + lastName + " and I am " + str(age) + " years old.")
